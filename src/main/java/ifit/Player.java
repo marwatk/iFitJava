23ba8b820c615c08279ec0de67a2489b5fb746fb
Marcus Watkins <me@marcuswatkins.net>
2019-10-26 12:30:33 -0600

    Remove cruft

--- src/main/java/ifit/Player.java
+++ src/main/java/ifit/Player.java
@@ -1,6 +1,10 @@
 package ifit;
 
-import javax.sound.sampled.*;
+import javax.sound.sampled.AudioFormat;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.DataLine;
+import javax.sound.sampled.LineUnavailableException;
+import javax.sound.sampled.SourceDataLine;
 
 public class Player {
 	private SourceDataLine line;
